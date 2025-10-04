b9245e61d2943a20f9a57d4753d871cf0cfd83c1
unknown <snehalthorat1512@gmail.com>
2025-10-03 21:24:13 -0500

    Setup SQLite DB and Create Student endpoint

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/snehal1512/students-api/internal/storage"
 	"github.com/snehal1512/students-api/internal/types"
 	"github.com/snehal1512/students-api/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		slog.Info("Creating student")
@@ -38,6 +39,19 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		slog.Info("User created successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
