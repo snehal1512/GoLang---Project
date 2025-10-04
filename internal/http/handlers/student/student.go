109cb25df2618a8bdd37586d178b2c8b53a12dd3
unknown <snehalthorat1512@gmail.com>
2025-10-03 22:25:04 -0500

    Added GetAllStudents API

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -81,3 +81,19 @@ func GetByID(storage storage.Storage) http.HandlerFunc {
 
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		slog.Info("Getting All Students")
+
+		students, err := storage.GetStudents()
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+	}
+}
